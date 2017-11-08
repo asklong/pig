3fa69a2d9feeb717c0d14f75ac9761735e93ee08
asklong <499671517@qq.com>
2017-11-07 16:38:56 -0800

    Create generate_pair_image.sh

---
+++ generate_pair_image.sh
@@ -0,0 +1,23 @@
+function generate_pair_image {
+    DAY=$1
+    INPUT_PATH=$2
+    SAVE_PATH=$3
+    SKU_INFO_PATH=$4
+    JOB_NAME=$5
+
+    hadoop fs -test -e ${SAVE_PATH}
+    if [ $? -eq 0 ]
+    then
+        hadoop fs -rmr ${SAVE_PATH}
+    fi
+
+    pig -useHCatalog \
+        -p day="${DAY}" \
+        -p input_path="${INPUT_PATH}" \
+        -p save_path="${SAVE_PATH}" \
+        -p sku_info_path="${SKU_INFO_PATH}" \
+        -p job_name="${JOB_NAME}" \
+        script/generate_pair_image.pig
+}
+
+#generate_pair_image "2017-08-04" "/user/jd_ad/ads_reco/xuzhexuan/multi-task_cnn/pairwise_data" "/user/jd_ad/ads_reco/xuzhexuan/multi-task_cnn/pairwise_data_image" "generate_pair_image"
